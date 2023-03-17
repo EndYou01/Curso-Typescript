89b1e07b3a64ef869a8d5b11d8fb57c68c662a7b
EndYou <jorgesenjudo@gmail.com>
2023-03-17 17:56:59 -0500

    Creando carpeta Counter

---
+++ src/counter/CounterApp.tsx
@@ -0,0 +1,16 @@
+import { Counter } from "./components/Counter"
+import { Usuario } from "./components/Usuario"
+
+
+export const CounterApp = () => {
+  return (
+    <div>
+        <h1>React + TypeScript</h1>
+        <hr />
+
+        <Counter/>
+
+        <Usuario/>
+    </div>
+  )
+}
